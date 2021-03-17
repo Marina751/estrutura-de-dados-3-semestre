06fdc99ae7d25cd3dfe1889e90742d5286055b14
Marina Gabriela <marina.gabriela.cardoso07@aluno.ifce.edu.br>
2021-03-16 22:47:42 -0300

    novo

---
+++ soma.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int A,B,X;
+    printf("Digite um numero: \n");
+    scanf("%d", &A);
+    printf("Digite um segundo numero: \n");
+    scanf("%d", &B);
+    X=A+B;
+    printf("A soma dos numeros é: %d", X);
+    getchar();
+}
