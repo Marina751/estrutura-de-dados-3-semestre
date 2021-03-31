ddff5b2fbe25c4e9247674cf2e19e5a44d081ee6
Marina Gabriela <marina.gabriela.cardoso07@aluno.ifce.edu.br>
2021-03-31 13:22:11 -0300

    exercicio 5.3 - pag 56

---
+++ exercicios-livro.c
@@ -0,0 +1,21 @@
+/*Codifique um programa para solicitar 5 números, via teclado, 
+e exibi-los na ordem inversa àquela em que foram fornecidos. */
+#include<stdio.h>
+int main (){
+    int vetor[5]={0};
+    for(int i=0; i<5; i++){
+        printf("Digite o numero: ");
+        scanf("%d", &vetor[i]);
+    }
+    printf("\n");
+
+    for(int i=0; i<5; i++){
+        printf("Voce digitou, em ordem de digitacao: %d\n", vetor[i]);
+    }
+    printf("\n");
+
+    for(int i=4; i>=0; i--){
+        printf("Voce digitou, em ordem inversa de digitacao: %d\n", vetor[i]);
+    }
+    return 0;
+}
